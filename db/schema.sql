3149fc7a9366952e86996502ef1c5fdd0fb6da90
milandiaz <milandiaz2000@hotmail.com>
2025-08-01 10:28:33 -0500

    First commit test with schema

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NULL,
+    images TEXT,
+    phone TEXT,
+    email TEXT
+);
+
+CREATE TABLE faculty (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    bioImage TEXT,
+    bioDescription TEXT,
+    department_id INTEGER REFERENCES  departments(id) ON DELETE CASCADE
+);
