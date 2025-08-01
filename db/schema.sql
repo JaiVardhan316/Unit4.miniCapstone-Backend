40ebde0460f7d611560f12c2987d78b19931f868
milandiaz <milandiaz2000@hotmail.com>
2025-08-01 11:29:49 -0500

    Added faculty to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
+DROP TABLE IF EXISTS admins;
 DROP TABLE IF EXISTS faculty;
 DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    description TEXT NULL,
+    description TEXT NOT NULL,
     images TEXT,
     phone TEXT,
     email TEXT
@@ -16,4 +17,10 @@ CREATE TABLE faculty (
     bioImage TEXT,
     bioDescription TEXT,
     department_id INTEGER REFERENCES  departments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE admins (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
 );
