fc9640c892b5e0bd6ffb70864ab70613589b1052
Jai Vardhan <pdmnvardhan510@gmail.com>
2025-08-04 09:08:24 -0500

    added router

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ import morgan from "morgan";
 import express from "express";
 const app = express();
 export default app;
+import facultyRouter from "#api/faculty";
 
 // middleware
 app.use(express.json());
 app.use(morgan("dev"));
+
+
+
+
+app.use("/faculty", facultyRouter);
+
 
 // GET / to send the message "Hello Lincoln!"
 app.route("/").get((req, res) => {
